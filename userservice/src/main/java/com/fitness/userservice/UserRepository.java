46cb84ead7d3dfdb98fe1c3651996837ae44f0ee
Yash Shelar <yashshelar006@gmail.com>
2026-04-05 19:57:23 +0530

    feat(userservice): implement user registration flow with controller, service, repository, and DTOs

---
+++ userservice/src/main/java/com/fitness/userservice/UserRepository.java
@@ -0,0 +1,10 @@
+package com.fitness.userservice;
+
+import com.fitness.userservice.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Boolean existsByEmail(String email);
+}
---
+++ userservice/src/main/java/com/fitness/userservice/services/UserService.java
@@ -0,0 +1,57 @@
+package com.fitness.userservice.services;
+
+import com.fitness.userservice.UserRepository;
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.models.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserResponse register(RegisterRequest request) {
+
+        if (repository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already exits");
+        }
+
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+
+        User savedUser = repository.save(user);
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(savedUser.getId());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setPassword(savedUser.getPassword());
+        userResponse.setFirstName(savedUser.getFirstName());
+        userResponse.setLastName(savedUser.getLastName());
+        userResponse.setCreatedAt(savedUser.getCreatedAt());
+        userResponse.setUpdatedAt(savedUser.getUpdatedAt());
+
+        return userResponse;
+    }
+
+    public UserResponse getUserProfile(String userId) {
+        User user = repository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User Not Found"));
+
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(user.getId());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setPassword(user.getPassword());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setCreatedAt(user.getCreatedAt());
+        userResponse.setUpdatedAt(user.getUpdatedAt());
+
+        return userResponse;
+
+    }
+}
