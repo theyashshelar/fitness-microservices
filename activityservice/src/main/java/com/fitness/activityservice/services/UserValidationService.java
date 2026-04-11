e316ce3f3e3dd247fe775a1d3fc215fdea09398d
Yash Shelar <yashshelar006@gmail.com>
2026-04-12 00:48:56 +0530

    Added AI recommendation service, gateway routing, and microservices integration

--- activityservice/src/main/java/com/fitness/activityservice/services/UserValidationService.java
+++ activityservice/src/main/java/com/fitness/activityservice/services/UserValidationService.java
@@ -1,20 +1,22 @@
 package com.fitness.activityservice.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserValidationService {
-
     private final WebClient userServiceWebClient;
 
     public boolean validateUser(String userId) {
+        log.info("Calling User Service for {}", userId);
         try {
             return userServiceWebClient.get()
-                    .uri("api/users/{userId}/validate", userId)
+                    .uri("/api/users/{userId}/validate", userId)
                     .retrieve()
                     .bodyToMono(Boolean.class)
                     .block();
